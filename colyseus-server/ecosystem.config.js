02be97b553ca009e2a2c024bfa642ac08ac69e1e
bitcointeamlt-blip <bitcointeamlt@gmail.com>
2025-11-25 15:19:59 +0100

    qqw1

---
+++ colyseus-server/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [{
+    name: 'colyseus-server',
+    script: 'build/index.js',
+    instances: 1,
+    exec_mode: 'fork',
+    wait_ready: true,
+    env: {
+      NODE_ENV: 'production',
+      PORT: process.env.PORT || 2567
+    },
+    autorestart: true,
+    watch: false
+  }]
+};
